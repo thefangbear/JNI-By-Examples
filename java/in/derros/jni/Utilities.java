29374a1f7af54c93a855be3fcdb17a9bedb6f7e7
Patrick Cox <pxcox003@odu.edu>
2019-03-05 18:18:58 -0500

    Allow use of static or dynamic linked libjnitests
    Allow use of a static or dynamically linked libjnitests based of file extension.
    Allow functionality for Windows or Linux based file extensions by using the "os.name" property.

--- java/in/derros/jni/Utilities.java
+++ java/in/derros/jni/Utilities.java
@@ -4,12 +4,41 @@ import java.nio.file.FileSystems;
 
 
 class Utilities {
+    // Ensure library is only loaded once
     static {
-        // Ensure library is only loaded once
-        System.load(
-            FileSystems.getDefault()
-                       .getPath("./../build/libjnitests.so")
-                       .normalize().toAbsolutePath().toString());
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            // Windows based
+            try {
+                System.load(
+                    FileSystems.getDefault()
+                            .getPath("./../build/libjnitests.dll")  // Dynamic link
+                            .normalize().toAbsolutePath().toString());
+            }
+            catch (UnsatisfiedLinkError e) {
+                System.load(
+                    FileSystems.getDefault()
+                            .getPath("./../build/libjnitests.lib")  // Static link
+                            .normalize().toAbsolutePath().toString());
+            }
+        }
+        else
+        {
+            // Unix based
+            try
+            {
+                System.load(
+                    FileSystems.getDefault()
+                            .getPath("./../build/libjnitests.so")  // Dynamic link
+                            .normalize().toAbsolutePath().toString());
+            }
+            catch (UnsatisfiedLinkError e)
+            {
+                System.load(
+                    FileSystems.getDefault()
+                            .getPath("./../build/libjnitests.a")  // Static link
+                            .normalize().toAbsolutePath().toString());
+            }
+        }
     }
 
     private native void printMethod();
